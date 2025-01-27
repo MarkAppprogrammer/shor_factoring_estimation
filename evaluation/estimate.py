8346b1401328304af5ef4adc915f3fbc31f783d3
Mark Agib <80441909+MarkAppprogrammer@users.noreply.github.com>
2025-01-26 23:37:52 -0800

    Updated output + Fixed code distance bug

--- evaluation/estimate.py
+++ evaluation/estimate.py
@@ -5,7 +5,7 @@ Last Updated: 1/26/2025
 @author: Mark Agib
 """
 
-import math
+from math import ceil, isnan, isinf, log
 import matplotlib.pyplot as plt
 import datetime
 
@@ -19,90 +19,90 @@ def estimate_best(n, num_mod_mult, gate_error):
    print(n)
    print(num_mod_mult)
    print(gate_error)
-
+"""
 def fips_strength_level(n):
     # From FIPS 140-2 IG CMVP, page 110.
     #
     # This is extrapolated from the asymptotic complexity of the sieving
     # step in the general number field sieve (GNFS).
-    ln = math.log
+    ln = log
     return (1.923 * (n * ln(2))**(1/3) * ln(n * ln(2))**(2/3) - 4.69) / ln(2)
 
 def fips_strength_level_rounded(n): # NIST-style rounding
     return 8 * round(fips_strength_level(n) / 8)
-"""
+
 
 # RSA removed  -> Optional[CostEstimate] from all below
-def eh_rsa(n, gate_error_rate): # Single run.
+def eh_rsa(n): # Single run.
     
     delta = 20 # Required to respect assumptions in the analysis.
-    m = math.ceil(n / 2) - 1
+    m = ceil(n / 2) - 1
     l = m - delta
     n_e = m + 2 * l
     return n_e #estimate_best(n, n_e, gate_error_rate)
 
-"""
-def eh_rsa_max_tradeoffs(n, gate_error_rate): # With maximum tradeoffs.
-    return estimate_best(n, math.ceil(n / 2), gate_error_rate)
+
+# def eh_rsa_max_tradeoffs(n, gate_error_rate): # With maximum tradeoffs.
+#     return estimate_best(n, math.ceil(n / 2), gate_error_rate)
 
 
-# General DLP
-def shor_dlp_general(n, gate_error_rate):
-    delta = 5 # Required to reach 99% success probability.
-    m = n - 1 + delta
-    n_e = 2 * m
-    return estimate_best(n, n_e, gate_error_rate)
+# # General DLP
+# def shor_dlp_general(n, gate_error_rate):
+#     delta = 5 # Required to reach 99% success probability.
+#     m = n - 1 + delta
+#     n_e = 2 * m
+#     return n_e
 
 
-def eh_dlp_general(n, gate_error_rate): # Single run.
-    m = n - 1
-    n_e = 3 * m
-    return estimate_best(n, n_e, gate_error_rate)
+# def eh_dlp_general(n, gate_error_rate): # Single run.
+#     m = n - 1
+#     n_e = 3 * m
+#     return n_e
 
 
-def eh_dlp_general_max_tradeoffs(n, gate_error_rate): # Multiple runs with maximal tradeoff.
-    m = n - 1
-    n_e = m
-    return estimate_best(n, n_e, gate_error_rate)
+# # def eh_dlp_general_max_tradeoffs(n, gate_error_rate): # Multiple runs with maximal tradeoff.
+# #     m = n - 1
+# #     n_e = m
+# #     return estimate_best(n, n_e, gate_error_rate)
 
 
-# Schnorr DLP
-def shor_dlp_schnorr(n, gate_error_rate):
-    delta = 5 # Required to reach 99% success probability.
-    z = fips_strength_level_rounded(n)
-    m = 2 * z + delta
-    n_e = 2 * m
-    return estimate_best(n, n_e, gate_error_rate)
+# # Schnorr DLP
+# def shor_dlp_schnorr(n, gate_error_rate):
+#     delta = 5 # Required to reach 99% success probability.
+#     z = fips_strength_level_rounded(n)
+#     m = 2 * z + delta
+#     n_e = 2 * m
+#     return n_e
 
 
-def eh_dlp_schnorr(n, gate_error_rate): # Single run.
-    z = fips_strength_level_rounded(n)
-    m = 2 * z
-    n_e = 3 * m
-    return estimate_best(n, n_e, gate_error_rate)
+# def eh_dlp_schnorr(n, gate_error_rate): # Single run.
+#     z = fips_strength_level_rounded(n)
+#     m = 2 * z
+#     n_e = 3 * m
+#     return n_e
 
 
-def eh_dlp_schnorr_max_tradeoffs(n, gate_error_rate): # Multiple runs with maximal tradeoff.
-    z = fips_strength_level_rounded(n)
-    m = 2 * z
-    n_e = m
-    return estimate_best(n, n_e, gate_error_rate)
+# # def eh_dlp_schnorr_max_tradeoffs(n, gate_error_rate): # Multiple runs with maximal tradeoff.
+# #     z = fips_strength_level_rounded(n)
+# #     m = 2 * z
+# #     n_e = m
+# #     return estimate_best(n, n_e, gate_error_rate)
 
 
-# Short DLP
-def eh_dlp_short(n, gate_error_rate):
-    z = fips_strength_level_rounded(n)
-    m = 2 * z
-    n_e = 3 * m
-    return estimate_best(n, n_e, gate_error_rate)
+# # Short DLP
+# def eh_dlp_short(n, gate_error_rate):
+#     z = fips_strength_level_rounded(n)
+#     m = 2 * z
+#     n_e = 3 * m
+#     return n_e
 
 
-def eh_dlp_short_max_tradeoffs(n, gate_error_rate): # Multiple runs with maximal tradeoff.
-    z = fips_strength_level_rounded(n)
-    m = 2 * z
-    n_e = m
-    return estimate_best(n, n_e, gate_error_rate)
-"""
+# def eh_dlp_short_max_tradeoffs(n, gate_error_rate): # Multiple runs with maximal tradeoff.
+#     z = fips_strength_level_rounded(n)
+#     m = 2 * z
+#     n_e = m
+#     return n_e
+
 
 #______Ancillary Functions______
 
@@ -111,7 +111,7 @@ def memory_limited_time(err_corr: CorrectionCode):
    """Cycle-time when limited by memory readout time."""
    # Time to access 1 qubit with 4Γ/10 hypothesis
    speed = 4 * 12e6 / 10  # qubit/s
-   return qubits_in_mem(err_corr, verb=False) / speed
+   return qubits_in_mem(err_corr) / speed
 
 def correct_all(err_corr: CorrectionCode):
    """Compute time for error-correcting all the memory."""
@@ -123,23 +123,24 @@ def modes_in_mem(err_corr: CorrectionCode):
 def qubits_in_mem(err_corr: CorrectionCode):
    return err_corr.memory_qubits*logical_qubit_count(err_corr.params)
 
-def logical_qubit_count(params: Params): ## figure out wear from + 
+def logical_qubit_count(params: params): ## figure out wear from + 
    if params.windowed:
-      count = 4 * params.n + 3 * params.m + params.windowed_exp - 1
+      count = 4 * params.n + 3 * params.deviation_padding + params.windowed_exp - 1
    else: 
-      count = 3*(params.n + params.m) + 1
+      count = 3*(params.n + params.deviation_padding) + 1
    return count
 
 #_______________________________
 
-def iterate(params: Params, **kwargs):
+def iterate(params: params, **kwargs):
    """kwargs can be passed ex: wes and wms"""
    if type(params.extra_opts) is gsOptions: ## need to add itarete for gate error and moduli + check if this works
       ranges = dict(d1s=(None,),
-                    ds=(1, 100, 2),
+                    ds=range(1, 100, 2),
                     wes=range(1, 10),
                     wms=range(1, 10),
                     ms=range(1, 40)) ## change padding? for runways
+      #print(ranges)
    else: ## add for None?
       raise ValueError("params.extra_opts not valid!")
    ranges.update(kwargs)
@@ -157,13 +158,13 @@ def iterate(params: Params, **kwargs):
 def rank(cost: PhysicalCost, qubits, bias=1):
    return cost.exp_t * qubits**bias  ## update cost.exp_t
 
-def prepare_resources(params: Params):
+def prepare_resources(params: params):
    err_corr = CorrectionCode(params) ## needs updating
    cost = err_corr.modular_exp()
    qubits = err_corr.proc_qubits
    return cost, qubits
 
-def find_best(beg_params: Params, bias=1):  ## cleared
+def find_best(beg_params: params, bias=1):  ## cleared
    best = float('inf') ## infiniti
    best_params = None
 
@@ -179,6 +180,43 @@ def find_best(beg_params: Params, bias=1):  ## cleared
    if best_params == None: ## wasn't updated
       print("An optmization didn't converge (ran out of time)")
    return best_params
+
+##_________________________________________________
+
+
+def unit_format(num, unit, unicode=False):
+   """Assemble number and unit, eventually converting it into LaTeX."""
+   space = chr(8239)
+   num = str(round(num)) if not isinf(num) else "∞" if unicode else r"\infty"
+   if not unicode:
+      unit = {"µs": r"\micro\second",
+               "ms": r"\milli\second",
+               "s": r"\second",
+               "min": r"\minute",
+               "hours": "hours",
+               "days": "days"}[unit]
+   if unicode:
+      return num + space + unit
+   return rf"\SI{{{num}}}{{{unit}}}"
+
+
+def format_time(time, unicode=False):
+   """Return formatted time, with correct unity."""
+   if isnan(time):
+      return "nan"
+   if time < 1e-3:
+      temps, unit = time*1e6, "µs"
+   elif time < 1:
+      temps, unit = time*1000, "ms"
+   elif time < 60:
+      temps, unit = time, "s"
+   elif time < 3600:
+      temps, unit = time/60, "min"
+   elif time < 3600*24:
+      temps, unit = time/3600, "hours"
+   else:
+      temps, unit = time/(3600*24), "days"
+   return unit_format(temps, unit, unicode)
 
 
 """
@@ -196,10 +234,25 @@ if __name__ == '__main__':
    #start
 
    #typing.NamedTuple doesn't offer defaults :(
-   params = params(n=2048, ne=eh_rsa(2048), windowed_exp=None, windowed_mult=None,
+   params = params(n=2048, n_e=eh_rsa(2048), windowed_exp=None, windowed_mult=None,
                     cycle_time=1e-6, reaction_time=1e-6, measure_based_uncomp=True,
                     windowed=True, deviation_padding=None, gate_error_rate=1e-3,
                     extra_opts=gsOptions(2, None, None)
     )
    
-   best_params = find_best(params, biais=1) # what is the bias for
+   print("Windowed arithmetics")
+   print("====================")
+   best_params = find_best(params)
+   best_err_corr = CorrectionCode(best_params)
+   cost, qubits = prepare_resources(best_params)
+   print("Best cost:", cost, ";",  qubits)
+   print("Max time between two reading:", best_err_corr.temps_inter_lectures())
+   print("In memory physical qubits:", qubits_in_mem(best_err_corr))
+   print("Full error-correction time:",
+         format_time(correct_all(best_err_corr), True))
+   print("Time if one spatial mode:", format_time(
+      cost.exp_t*memory_limited_time(best_err_corr)/best_params.cycle_time,
+      unicode=True))
+
+   
+   
